a95e9e814609b256be9e5ce6451a5d35bdc7275d
madhes <v.madheswaran5@gmail.com>
2024-03-20 09:31:00 +0530

    inital render

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 function App() {
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const submit = () => {
+    console.log("click");
+    const myuser = [{ name: name, email: email }];
+    setData([data, ...myuser]);
+  };
   return (
     <>
       <h3>Welcome</h3>
+
+      {data
+        ? data.map((d, i) => {
+            return <li key={i}>{d.name}</li>;
+          })
+        : ""}
+
+      <input
+        type="text"
+        placeholder="name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      ></input>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      ></input>
+
+      <button onClick={submit}>Submit</button>
     </>
   );
 }
